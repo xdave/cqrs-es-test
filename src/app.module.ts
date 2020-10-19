e5aefb1bbacf0cc607df58160934819d29aca465
xdave <xdave@users.noreply.github.com>
2020-10-19 14:22:53 -0600

    adjust imports of app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { CoreModule } from "./core/core.module";
-import { ChangeUserNameHandler } from "./user/commands/change-user-name/change-user-name.handler";
-import { CreateUserHandler } from "./user/commands/create-user/create-user.handler";
-import { InMemoryUserRepository } from "./user/repository/user.repository.in-memory";
-import { IUserRepository } from "./user/repository/user.repository.interface";
 import { NotificationsModule } from "./notifications/notifications.module";
 import { UserModule } from "./user/user.module";
 
